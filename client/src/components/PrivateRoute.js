73d5ddfe9cac19fc8589cc50df7efba7de671679
lilian <lilliannzioki@gmail.com>
2024-07-15 18:13:59 +0300

    Ready for deployment

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+const PrivateRoute = ({ element, user }) => {
+  return user ? element : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
